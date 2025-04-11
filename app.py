b1040c29c8a2ed30c74323c04071825eeec92cb6
Pawan Shinde <shindepawan1234@gmail.com>
2025-04-11 14:45:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-print("we are currently working get back to you soon")
+import streamlit as st
+
+st.set_page_config(page_title="🚧 Under Construction | SmartCapital", layout="centered")
+
+st.markdown("# 🚧 SmartCapital Web App")
+st.markdown("## We're currently working on something awesome!")
+st.info("Please check back soon. The app is under construction 👷‍♂️")
+
 
 
